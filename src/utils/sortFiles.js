3e11f31b1bc087dacc2ae073feb8afa009438632
Nahuel Burdisso <nahuelburdisso@example.com>
2022-03-15 18:08:21 -0300

    Update install with yarn

--- src/utils/sortFiles.js
+++ src/utils/sortFiles.js
@@ -1,6 +1,6 @@
 // Sorts files by asc and returns the files sorted with an the way they were sorted
 export const sortFiles = (files, asc) => {
-    const sortedFiles = [].concat(files);
+    const sortedFiles = [...files];
     if (asc) {
       sortedFiles.sort((a, b) => a.versions[0].name.localeCompare(b.versions[0].name));
     } else {
